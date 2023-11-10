ab3b2b64ffa529476ed53d0aa34117fba5469d42
Arihant <arihantjain72000@gmail.com>
2023-11-10 12:44:36 +0530

    first-commit

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <small>
+        Coded with ❤️ by{" "}
+        <a
+          className="footer--link"
+          href="https://www.instagram.com/im__arihantjain/?hl=en"
+          target="_blank"
+          rel="noreferrer"
+        >
+          ARIHANT JAIN
+        </a>
+        .
+      </small>
+    </footer>
+  );
+}
